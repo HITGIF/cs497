a9e4f9c305fcecc6e69aa161e2b80ccfec6adcd3
Mark Wang <carbonylgp@gmail.com>
2022-04-11 17:31:23 -0400

    footer

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -4,10 +4,12 @@ import React from "react";
 export const Footer = () => {
   return (
     <Stack alignItems={"center"} spacing={2} p={6} sx={{bgcolor: "primary.contrastText"}}>
-      <Typography variant={"caption"} color={"white"}>
+      <Typography variant={"caption"} color={"white"} align={"center"}>
         This is a CS 497 project aimed to create a platform
-        for anonymously sharing job applications outcome. Currently the website
-        is populated with randomly generated data for demonstration purpose only.
+        for anonymously sharing job applications outcome.
+        <br/>
+        Currently the website is populated with randomly generated data for demonstration purpose only.
+        <br/>
         Functionalities are complete but for security and legal reasons please do not submit
         real data yet.
       </Typography>
